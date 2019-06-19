1bc102d2dc8a0f22eeb78b6b3e2a475921f1c338
Roy <16163569+roytanjx@users.noreply.github.com>
2019-06-19 19:49:14 -0400

    Add profile page, axios, file selector, image uploader, various component skeleton

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import ProfilePage from './components/ProfilePage'
 import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
 // import SignUp from './components/SignUp';
 // import SignIn from './components/SignIn';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ProfilePage />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
