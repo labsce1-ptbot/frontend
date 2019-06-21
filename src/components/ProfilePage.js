5e623be3909dfd7d588a4ebe64b542e87bbb575d
Roy <16163569+roytanjx@users.noreply.github.com>
2019-06-20 20:53:52 -0400

    hide file input, adjusted div positions

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -20,37 +20,44 @@ class ProfilePage extends Component {
 		//send http request for upload
 		const profilePic = new FormData();
 		profilePic.append('image', this.state.selectedFile, this.state.selectedFile.name);
-        axios.post('', profilePic)
-        .then(res => {
-            console.log(res)
-        });
+		axios.post('', profilePic)
+		.then((res) => {
+			console.log(res);
+		});
 	};
 
 	render() {
 		return (
-			<div>
+            <div style={{ border: "2px solid red" }}>
 				<div className="schedulevacation">
 					<Button variant="primary">Schedule Vacation</Button>
 				</div>
-				<div>
-					{/* className for styling? */}
-					<div>
+                <div className="profilepic">
 						{/* upload profile picture */}
-						<input type="file" onChange={this.fileSelectedHandler} />
-						profile picture
-						<button onClick={this.fileUploadHandler}>Upload</button>
-					</div>
-					<div> username first/last </div>
-					<div className="connectprofile">
-						<ConnectProfile />
-					</div>
-					<div>Scheduled vacations show here</div>
-					<div>Scheduled vacations show here</div>
-					<div>Scheduled vacations show here</div>
-					<div>Scheduled vacations show here</div>
+					<input
+						// Trigger hide input when Select Picture is clicked
+						style={{ display: 'none' }}
+						type="file"
+						onChange={this.fileSelectedHandler}
+						// Bind property of class to reference of input
+						ref={fileInput => this.fileInput = fileInput} 
+					/>
+					<button onClick={() => this.fileInput.click()}>Select Picture</button>
+					profile picture
+					<button onClick={this.fileUploadHandler}>Upload</button>
 				</div>
+				<div className="username"> username first/last </div>
+				<div className="connectprofile">
+					<ConnectProfile />
+				</div>
+				<div className="vacationcontainer">
+					<div>Scheduled vacations show here</div>
+					<div>Scheduled vacations show here</div>
+					<div>Scheduled vacations show here</div>
+					<div>Scheduled vacations show here</div>
+                </div>
 			</div>
-		);
+        );
 	}
 }
 
