e3b1957d8527c221d805b0c411e4ccb68a505c08
Ta1grr <taile602@gmail.com>
2019-06-26 23:28:51 -0500

    got the landing page in, trying to debug to get it working

--- src/index.js
+++ src/index.js
@@ -3,23 +3,32 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
+import App from "./App";
+
 // core components
-import Admin from "./layouts/Admin.jsx";
-import Auth from "./layouts/Auth.jsx";
-import RTL from "./layouts/RTL.jsx";
+// import Admin from "./layouts/Admin.jsx";
+// import Auth from "./layouts/Auth.jsx";
+// import RTL from "./layouts/RTL.jsx";
 
 import "./assets/css/material-dashboard-react.css?v=1.6.0";
 
 const hist = createBrowserHistory();
 
+// ReactDOM.render(
+//   <Router history={hist}>
+//     <Switch>
+//       <Route path="/admin" component={Admin} />
+//       <Route path="/auth" component={Auth} />
+//       <Route path="/rtl" component={RTL} />
+//       <Redirect from="/" to="/admin/dashboard" />
+//     </Switch>
+//   </Router>,
+//   document.getElementById("root")
+// );
+
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/auth" component={Auth} />
-      <Route path="/rtl" component={RTL} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
+  <Router>
+    <App />
   </Router>,
   document.getElementById("root")
 );
