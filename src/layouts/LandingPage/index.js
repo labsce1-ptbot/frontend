8f27d14053a73a2db08bb01ab68f722b2e69b6b6
Roy <16163569+roytanjx@users.noreply.github.com>
2019-07-02 19:09:35 -0400

    adjusted homepage opacity, responsivedesign

--- src/layouts/LandingPage/index.js
+++ src/layouts/LandingPage/index.js
@@ -1,75 +1,58 @@
-import React from "react";
-import "./assets/css/main.css";
-import "./assets/css/fontawesome-all.min.css";
+import React from 'react';
+import './assets/css/main.css';
+import './assets/css/fontawesome-all.min.css';
 
 class LandingPage extends React.Component {
-  state = {};
+	state = {};
 
-  render() {
-    return (
-      <div>
-        {/* //<!-- Header --> */}
-        <div className="header">
-          <h1>Eventually</h1>
-          <p>
-            A simple template for telling the world when you'll launch
-            <br />
-            your next big thing. Brought to you by
-            <a href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}>
-              Start here!
-            </a>
-            .
-          </p>
-        </div>
+	render() {
+		return (
+			<div>
+				{/* //<!-- Header --> */}
+				<div className="header">
+					<h1>PTbOt</h1>
+					<p>Sign up and start scheduling your vacations!</p>
+				</div>
 
-        {/* //<!-- Signup Form --> */}
-        <form id="signup-form" method="post" action="#">
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email Address"
-          />
-          {/* <a href={`https://pt-b0t.herokuapp.com/auth/login`}>Start Now!</a> */}
-          <a href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}><strong>Start Now!</strong></a>
-        </form>
+				{/* //<!-- Signup Form --> */}
+				<form id="signup-form" method="post" action="#">
+					<input type="email" name="email" id="email" placeholder="Email Address" />
+					{/* <a href={`https://pt-b0t.herokuapp.com/auth/login`}>Start Now!</a> */}
+					<a href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}>
+						<strong>Start Now!</strong>
+					</a>
+				</form>
 
-        {/* //<!-- Footer --> */}
-        <div className="footer">
-          <ul class="icons">
-            {/* <React.Fragment className="icons"> */}
-            <li>
-              <a href="##" className="icon brands fa-twitter">
-                <span class="label">Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a href="##" className="icon brands fa-instagram">
-                <span class="label">Instagram</span>
-              </a>
-            </li>
-            <li>
-              <a href="##" className="icon brands fa-github">
-                <span class="label">GitHub</span>
-              </a>
-            </li>
-            <li>
-              <a href="##" className="icon fa-envelope">
-                <span class="label">Email</span>
-              </a>
-            </li>
-            {/* </React.Fragment> */}
-          </ul>
-          <ul className="copyright">
-            <li>&copy; Untitled.</li>
-            <li>
-              Credits: <a href="http://html5up.net">HTML5 UP</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
-  }
+				{/* //<!-- Footer --> */}
+				<div className="footer">
+					<ul class="icons">
+						{/* <React.Fragment className="icons"> */}
+						<li>
+							<a href="##" className="icon brands fa-twitter">
+								<span class="label">Twitter</span>
+							</a>
+						</li>
+						<li>
+							<a href="##" className="icon brands fa-instagram">
+								<span class="label">Instagram</span>
+							</a>
+						</li>
+						<li>
+							<a href="##" className="icon brands fa-github">
+								<span class="label">GitHub</span>
+							</a>
+						</li>
+						<li>
+							<a href="##" className="icon fa-envelope">
+								<span class="label">Email</span>
+							</a>
+						</li>
+						{/* </React.Fragment> */}
+					</ul>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default LandingPage;
