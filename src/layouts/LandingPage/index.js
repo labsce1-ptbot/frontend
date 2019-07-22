f9f4f1aa80d87480ffda5637788e36c6e751b254
Brian G <brian.gregorius@gmail.com>
2019-07-22 09:54:13 -0400

    changed landingpage

--- src/layouts/LandingPage/index.js
+++ src/layouts/LandingPage/index.js
@@ -1,58 +1,145 @@
-import React from 'react';
-import './assets/css/main.css';
-import './assets/css/fontawesome-all.min.css';
+import React from "react";
+import "./assets/css/main.css";
+import "./assets/css/fontawesome-all.min.css";
+import CardBody from "../../components/Card/CardBody";
+import Card from "../../components/Card/Card";
+import GridContainer from "../../components/Grid/GridContainer";
+import GridItem from "../../components/Grid/GridItem.jsx";
+import logo from "../../assets/img/vacalogo.png";
+import Button from "../../components/CustomButtons/Button";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CardHeader from "../../components/Card/CardHeader";
+import "../../assets/css/calendar.css";
+
+const styles = {
+  joinBtn: {
+    color: "white"
+  },
+  signUpBTN: {
+    textAlign: "center"
+  },
+
+  logo: {
+    height: "100px"
+  },
+
+  btnDiv: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center"
+  },
+
+  homePageCard: {
+    background: "rgba(255, 255, 255, 0.8)"
+  }
+};
 
 class LandingPage extends React.Component {
-	state = {};
+  state = {};
 
-	render() {
-		return (
-			<div>
-				{/* //<!-- Header --> */}
-				<div className="header">
-					<h1>PTbOt</h1>
-					<p>Sign up and start scheduling your vacations!</p>
-				</div>
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={8}>
+            {/* //<!-- Header --> */}
+            <div className="header">
+              <img src={logo} alt="PTbOt logo" className={classes.logo} />
+              <h1>PTbOt</h1>
+              <p>Sign up and start scheduling your vacations!</p>
+            </div>
 
-				{/* //<!-- Signup Form --> */}
-				<form id="signup-form" method="post" action="#">
-					<input type="email" name="email" id="email" placeholder="Email Address" />
-					{/* <a href={`https://pt-b0t.herokuapp.com/auth/login`}>Start Now!</a> */}
-					<a href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}>
-						<strong>Start Now!</strong>
-					</a>
-				</form>
+            {/* //<!-- Signup Form --> */}
+            {/* <form id="signup-form" method="post" action="#">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email Address"
+              /> */}
+            {/* <a href={`https://pt-b0t.herokuapp.com/auth/login`}>Start Now!</a> */}
+            {/* <a href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}>
+                <strong>Start Now!</strong>
+              </a>
+            </form> */}
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <Card className={classes.homePageCard}>
+              <CardHeader color="primary">
+                <h1>PTbOt: Slack Bot</h1>
+              </CardHeader>
+              <CardBody className={classes.listItems}>
+                <ul className={classes.listItems}>
+                  <li className="listItems">
+                    Schedule away messages for Slack
+                  </li>
+                  <li className="listItems">
+                    PTbOt responds to anyone that mentions you while you're away
+                  </li>
+                  <li className="listItems">
+                    Create custom away messages for all uses or target a
+                    specific group/user
+                  </li>
+                  <li className="listItems">
+                    <b>
+                      PTbOt does not save, store or read any of your Slack
+                      messages
+                    </b>
+                  </li>
+                  <li className="listItems">
+                    All of your stored vacations are removed from our database
+                    the day after your vacation ends
+                  </li>
+                  <li className="listItems">
+                    PTbOt is completely open source and free to use
+                  </li>
+                </ul>
+                <div className={classes.btnDiv}>
+                  <a
+                    href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}
+                    className={classes.signUpBTN}
+                  >
+                    <Button color="primary" round>
+                      <p className={classes.joinBtn}>Signup/SignIn</p>
+                    </Button>
+                  </a>
+                </div>
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
 
-				{/* //<!-- Footer --> */}
-				<div className="footer">
-					<ul class="icons">
-						{/* <React.Fragment className="icons"> */}
-						<li>
-							<a href="##" className="icon brands fa-twitter">
-								<span class="label">Twitter</span>
-							</a>
-						</li>
-						<li>
-							<a href="##" className="icon brands fa-instagram">
-								<span class="label">Instagram</span>
-							</a>
-						</li>
-						<li>
-							<a href="##" className="icon brands fa-github">
-								<span class="label">GitHub</span>
-							</a>
-						</li>
-						<li>
-							<a href="##" className="icon fa-envelope">
-								<span class="label">Email</span>
-							</a>
-						</li>
-						{/* </React.Fragment> */}
-					</ul>
-				</div>
-			</div>
-		);
-	}
+        {/* //<!-- Footer --> */}
+        <div className="footer">
+          <ul class="icons">
+            {/* <React.Fragment className="icons"> */}
+            <li>
+              <a href="##" className="icon brands fa-twitter">
+                <span class="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a href="##" className="icon brands fa-instagram">
+                <span class="label">Instagram</span>
+              </a>
+            </li>
+            <li>
+              <a href="##" className="icon brands fa-github">
+                <span class="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a href="##" className="icon fa-envelope">
+                <span class="label">Email</span>
+              </a>
+            </li>
+            {/* </React.Fragment> */}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default LandingPage;
+export default withStyles(styles)(LandingPage);
