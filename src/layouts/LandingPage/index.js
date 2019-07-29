da884d81ee598a1139a6ed851e56427268936fc6
Brian G <brian.gregorius@gmail.com>
2019-07-29 19:57:35 -0400

    rearranged homepage

--- src/layouts/LandingPage/index.js
+++ src/layouts/LandingPage/index.js
@@ -10,6 +10,7 @@ import Button from "../../components/CustomButtons/Button";
 import withStyles from "@material-ui/core/styles/withStyles";
 import CardHeader from "../../components/Card/CardHeader";
 import "../../assets/css/calendar.css";
+import background from "../LandingPage/images/bg07.svg";
 
 const styles = {
   joinBtn: {
@@ -30,7 +31,13 @@ const styles = {
   },
 
   homePageCard: {
-    background: "rgba(255, 255, 255, 0.8)"
+    // background: "rgba(255, 255, 255, 0.8)"
+    width: "500px",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    height: "100vh",
+    marginTop: 0
   },
 
   homePageHeader: {
@@ -45,77 +52,62 @@ class LandingPage extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={8}>
-            {/* //<!-- Header --> */}
-            <div className="header">
-              <img src={logo} alt="PTbOt logo" className={classes.logo} />
-              <h1>PTbOt</h1>
-              <p>Sign up and start scheduling your vacations!</p>
-            </div>
+      <div className="landingpage">
+        {/* //<!-- Header --> */}
+        {/* <img src={background} alt="vacation beach" /> */}
 
-            {/* //<!-- Signup Form --> */}
-            {/* <form id="signup-form" method="post" action="#">
+        {/* //<!-- Signup Form --> */}
+        {/* <form id="signup-form" method="post" action="#">
               <input
                 type="email"
                 name="email"
                 id="email"
                 placeholder="Email Address"
               /> */}
-            {/* <a href={`https://pt-b0t.herokuapp.com/auth/login`}>Start Now!</a> */}
-            {/* <a href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}>
+        {/* <a href={`https://pt-b0t.herokuapp.com/auth/login`}>Start Now!</a> */}
+        {/* <a href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}>
                 <strong>Start Now!</strong>
               </a>
             </form> */}
-          </GridItem>
-          <GridItem xs={12} sm={12} md={4}>
-            <Card className={classes.homePageCard}>
-              <CardHeader color="primary" className={classes.homePageHeader}>
-                <img src={logo} alt="PTbOt logo" className={classes.logo} />
-                <h1>PTbOt</h1>
-              </CardHeader>
-              <CardBody className={classes.listItems}>
-                <ul className={classes.listItems}>
-                  <li className="listItems">
-                    Schedule away messages for Slack
-                  </li>
-                  <li className="listItems">
-                    PTbOt responds to anyone that mentions you while you're away
-                  </li>
-                  <li className="listItems">
-                    Create custom away messages for all users or target a
-                    specific group/user
-                  </li>
-                  <li className="listItems">
-                    <b>
-                      PTbOt does not save, store and/or read any of your Slack
-                      messages
-                    </b>
-                  </li>
-                  <li className="listItems">
-                    All of your stored vacations are removed from our database
-                    the day after your vacation ends
-                  </li>
-                  <li className="listItems">
-                    PTbOt is completely open source and free to use
-                  </li>
-                  <li className="listItems">Google calendar integration</li>
-                </ul>
-                <div className={classes.btnDiv}>
-                  <a
-                    href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}
-                    className={classes.signUpBTN}
-                  >
-                    <Button color="primary" round>
-                      <p className={classes.joinBtn}>Signup/SignIn</p>
-                    </Button>
-                  </a>
-                </div>
-              </CardBody>
-            </Card>
-          </GridItem>
-        </GridContainer>
+
+        <Card className={classes.homePageCard}>
+          <CardBody className={classes.listItems}>
+            <ul className={classes.listItems}>
+              <li className="listItems">Schedule away messages for Slack</li>
+              <li className="listItems">
+                PTbOt responds to anyone that mentions you while you're away
+              </li>
+              <li className="listItems">
+                Create custom away messages for all users or target a specific
+                group/user
+              </li>
+              <li className="listItems">
+                <b>
+                  PTbOt does not save, store and/or read any of your Slack
+                  messages
+                </b>
+              </li>
+              <li className="listItems">
+                All of your stored vacations are removed from our database the
+                day after your vacation ends
+              </li>
+              <li className="listItems">
+                PTbOt is completely open source and free to use
+              </li>
+              <li className="listItems">Google calendar integration</li>
+            </ul>
+            <div className={classes.btnDiv}>
+              <a
+                href={`${process.env.REACT_APP_SERVER_URL}/auth/login`}
+                className={classes.signUpBTN}
+              >
+                <Button color="primary" round>
+                  <p className={classes.joinBtn}>Signup/SignIn</p>
+                </Button>
+              </a>
+            </div>
+          </CardBody>
+        </Card>
 
         {/* //<!-- Footer --> */}
         <div className="footer">
